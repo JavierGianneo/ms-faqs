5cfddc5ca9411c6e03bc6656e6c3631facc88eb3
Lenovo <javier.gianneo@gmail.com>
2021-08-02 19:05:41 -0300

    Starting

---
+++ src/main/java/com/example/msfaqs/MsFaqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.msfaqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFaqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFaqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msfaqs/MsFaqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msfaqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFaqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
